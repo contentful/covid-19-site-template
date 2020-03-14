7909ac2ad8bb12020d1e53f2ed32567bc4017748
Khaled Garbaya <khaledgarbaya@gmail.com>
2020-03-14 18:45:10 +0100

    feat: Add tailwindcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,14 @@ module.exports = {
     'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        tailwind: true,
+        purgeOnly: ['src/styles/tailwind.css'],
+      },
+    },
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-source-covid19',
