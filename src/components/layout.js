4cd44c6f5ed1b7b334f9c3574e325b757fb82a3a
Khaled Garbaya <khaled@contentful.com>
2020-03-15 20:20:00 +0100

    style: render img

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,9 @@ import '../styles/tailwind.css'
 import Stat from './stat'
 import TagList from './tagList'
 import FeaturedArticles from './featuredArticles'
-import ArticleTeaserList from '../components/articleTeaserList'
-import { Link, useStaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 export default ({ children }) => {
-  const { stats, teasers, tags, featuredArticles } = useStaticQuery(graphql`
+  const { stats, tags, featuredArticles } = useStaticQuery(graphql`
     {
       stats: allCovid19CountrySummary {
         nodes {
@@ -37,7 +36,7 @@ export default ({ children }) => {
   return (
     <>
       <header className="relative pb-24 bg-indigo-500">
-        <div className="p-8 flex flex-wrap items-center justify-center container mx-auto max-w-screen-xl">
+        <div className="container flex flex-wrap items-center justify-center p-8 mx-auto max-w-screen-xl">
           {stats.nodes.map(summary => (
             <Stat summary={summary} />
           ))}
@@ -47,14 +46,14 @@ export default ({ children }) => {
           width="1440"
           height="74"
           viewBox="0 0 1440 74"
-          className="fill-current text-white absolute bottom-0 w-full"
+          className="absolute bottom-0 w-full text-white fill-current"
         >
           <path d="M456.464 0.0433865C277.158 -1.70575 0 50.0141 0 50.0141V74H1440V50.0141C1440 50.0141 1320.4 31.1925 1243.09 27.0276C1099.33 19.2816 1019.08 53.1981 875.138 50.0141C710.527 46.3727 621.108 1.64949 456.464 0.0433865Z"></path>
         </svg>
       </header>
-      <div className="flex flex-wrap px-16 py-24 container max-w-screen-xl mx-auto">
-        <div className="w-full md:w-2/3">{children}</div>
-        <div className="w-full md:w-1/3">
+      <div className="flex flex-wrap px-8 py-24 container max-w-screen-xl mx-auto">
+        <div className="w-full p-8 md:w-2/3">{children}</div>
+        <div className="w-full p-8 md:w-1/3">
           <TagList tags={tags.nodes} />
           <FeaturedArticles
             featuredArticles={featuredArticles.nodes[0].blogPosts}
--- src/components/tagList.js
+++ src/components/tagList.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import Tag from '../components/Tag'
 
-export default ({ children, tags }) => (
-  <section className="min-h-full">
+export default ({ tags }) => (
+  <section className="mt-8">
     <h3 className="uppercase text-xl semi-bold"> Top Gategories </h3>
     <div className="mt-6">
-      {
-        tags.map(tag => (
-          <Tag tag={tag} key={tag.contentful_id} />
-        ))
-      }
+      {tags.map(tag => (
+        <Tag tag={tag} key={tag.contentful_id} />
+      ))}
     </div>
   </section>
 )
-
