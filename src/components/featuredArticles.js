a69b24fed88f5e809da9addfbe665fd88f2b7077
Khaled Garbaya <khaled@contentful.com>
2020-03-15 20:20:00 +0100

    style: render img

--- src/components/featuredArticles.js
+++ src/components/featuredArticles.js
@@ -2,13 +2,18 @@ import React from 'react'
 import FeaturedArticleLink from '../components/featuredArticleLink'
 
 export default ({ children, featuredArticles }) => (
-  <section className="mt-8 min-h-full">
+  <section className="mt-8">
     <h3 className="uppercase text-xl semi-bold">Featured Articles</h3>
     <div className="mt-6">
       <ul>
-        {
-          featuredArticles.map(featuredArticle => <li key={featuredArticle.contentful_id}><FeaturedArticleLink featuredArticle={featuredArticle} key={featuredArticle.contentful_id} /></li>)
-        }
+        {featuredArticles.map(featuredArticle => (
+          <li key={featuredArticle.contentful_id}>
+            <FeaturedArticleLink
+              featuredArticle={featuredArticle}
+              key={featuredArticle.contentful_id}
+            />
+          </li>
+        ))}
       </ul>
     </div>
   </section>
--- src/utils/rich-text-render-options.js
+++ src/utils/rich-text-render-options.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import { BLOCKS } from '@contentful/rich-text-types'
+import { BLOCKS, INLINES } from '@contentful/rich-text-types'
 
 const Text = ({ children }) => (
-  <p className="font-sans text-lg my-2">{children}</p>
+  <p className="my-2 font-sans text-lg">{children}</p>
 )
 const H1 = ({ children }) => <h1 className="font-sans text-3xl">{children}</h1>
 const H2 = ({ children }) => <h2 className="font-sans text-3xl">{children}</h2>
 const H3 = ({ children }) => <h3 className="font-sans text-3xl">{children}</h3>
-const H4 = ({ children }) => <h4 className="text-3xl font-sans">{children}</h4>
+const H4 = ({ children }) => <h4 className="font-sans text-3xl">{children}</h4>
+const HYPERLINK = ({ children }) => (
+  <a className="text-grey-darker hover:text-black text-sm no-underline hover:underline">
+    {children}
+  </a>
+)
+const Img = ({ node }) =>
+  console.log(node) || (
+    <img
+      src={node.data.target.fields.file['en-US'].url}
+      alt={node.data.target.fields.title['en-US']}
+    />
+  )
 const options = {
   renderNode: {
     [BLOCKS.PARAGRAPH]: (_node, children) => <Text>{children}</Text>,
@@ -15,6 +27,8 @@ const options = {
     [BLOCKS.HEADING_2]: (_node, children) => <H2>{children}</H2>,
     [BLOCKS.HEADING_3]: (_node, children) => <H3>{children}</H3>,
     [BLOCKS.HEADING_4]: (_node, children) => <H4>{children}</H4>,
+    [BLOCKS.EMBEDDED_ASSET]: node => <Img node={node} />,
+    [INLINES.HYPERLINK]: (_node, children) => <HYPERLINK>{children}</HYPERLINK>,
   },
   renderText: text => text.replace('!', '?'),
 }
