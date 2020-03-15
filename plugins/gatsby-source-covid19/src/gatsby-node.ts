ff2d0a69b1e2d0a79e1ab9a8799e62f7b3a287e9
Bruce Felt <bruce.felt@contentful.com>
2020-03-15 10:53:15 +0100

    refactor: Move country resolution to its own module

--- plugins/gatsby-source-covid19/src/gatsby-node.ts
+++ plugins/gatsby-source-covid19/src/gatsby-node.ts
@@ -1,45 +1,13 @@
 import { SourceNodesArgs } from 'gatsby'
 import APIClient from './api-client'
-import { toCountrySummaryNode } from './nodes'
+import resolveCountries from './nodes/countries'
+import { Covid19PluginOptions } from './nodes/types'
 
-interface CountryConfiguration {
-  iso2: string
-}
-
-interface PluginOptions {
-  baseURL?: string
-  countries: CountryConfiguration[]
-}
-
-export const onPreInit = () => {
-  console.log('Loaded gatsby-source-covid19')
-}
-
-export const sourceNodes = (kit: SourceNodesArgs, pluginOptions: PluginOptions) => {
-  const { actions: { createNode } } = kit
-
+export const sourceNodes = async (nodeKit: SourceNodesArgs, pluginOptions: Covid19PluginOptions) => {
   const apiClient = new APIClient({
     baseURL: pluginOptions.baseURL,
   })
 
-  const countries = pluginOptions.countries || []
-  const promises = countries.map(async ({ iso2 }) => {
-    let result
-    try {
-      result = await apiClient.countries.getSummary({
-        country: iso2,
-      })
-    } catch (e) {
-      console.log(e)
-    }
-    if (!result) {
-      return
-    }
-    const node = toCountrySummaryNode(kit, iso2, result)
-
-    createNode(node)
-  })
-
-  return Promise.all(promises)
+  await resolveCountries({ nodeKit, pluginOptions, apiClient })
 }
 
