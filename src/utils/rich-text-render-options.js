d0a81b1d7c174f6ecf3954a686cd73d182196461
Moe Shaaban <info@mshaaban.com>
2020-03-17 19:45:55 +0100

    fix: rich-text hyperlink

--- src/utils/rich-text-render-options.js
+++ src/utils/rich-text-render-options.js
@@ -4,12 +4,12 @@ import { BLOCKS, INLINES } from '@contentful/rich-text-types'
 const Text = ({ children }) => (
   <p className="my-2 font-sans text-lg">{children}</p>
 )
-const H1 = ({ children }) => <h1 className="font-sans text-3xl">{children}</h1>
-const H2 = ({ children }) => <h2 className="font-sans text-3xl">{children}</h2>
-const H3 = ({ children }) => <h3 className="font-sans text-3xl">{children}</h3>
-const H4 = ({ children }) => <h4 className="font-sans text-3xl">{children}</h4>
-const HYPERLINK = ({ children }) => (
-  <a className="text-grey-darker hover:text-black text-sm no-underline hover:underline">
+const H1 = ({ children }) => <h1 className="font-sans text-2xl font-semi-bold">{children}</h1>
+const H2 = ({ children }) => <h2 className="font-sans text-xl font-semi-bold">{children}</h2>
+const H3 = ({ children }) => <h3 className="font-sans text-lg font-semi-bold">{children}</h3>
+const H4 = ({ children }) => <h4 className="font-sans text-base font-semi-bold">{children}</h4>
+const HYPERLINK = ({ children, dataUri }) => (
+  <a className="text-blue-700 hover:text-blue-900 underline text-sm" href={dataUri}>
     {children}
   </a>
 )
@@ -28,7 +28,7 @@ const options = {
     [BLOCKS.HEADING_3]: (_node, children) => <H3>{children}</H3>,
     [BLOCKS.HEADING_4]: (_node, children) => <H4>{children}</H4>,
     [BLOCKS.EMBEDDED_ASSET]: node => <Img node={node} />,
-    [INLINES.HYPERLINK]: (_node, children) => <HYPERLINK>{children}</HYPERLINK>,
+    [INLINES.HYPERLINK]: (_node, children) => <HYPERLINK dataUri={_node.data && _node.data.uri}>{children}</HYPERLINK>,
   },
   renderText: text => text.replace('!', '?'),
 }
