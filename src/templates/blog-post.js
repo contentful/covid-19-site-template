50fb6d5cae5db5231a5b92f2f0958cacc2f325ed
Khaled Garbaya <khaled@contentful.com>
2020-03-17 15:58:03 +0100

    feat: Add SEO component

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,13 +4,36 @@ import Layout from '../components/layout'
 import TimeTag from '../components/timeTag'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import RichTextRenderOptions from '../utils/rich-text-render-options'
+import SEO from 'react-seo-component'
+
 export default ({ data }) => {
   return (
     <Layout>
-      <h1 className="text-3xl semi-bold mb-4">{data.contentfulBlogPost.title}</h1>
+      <SEO
+        title={data.contentfulBlogPost.title}
+        titleTemplate={`Stop the covid-19 spread`}
+        description={data.contentfulBlogPost.teaser.teaser}
+        image={image}
+        pathname={siteUrlPlusSlug}
+        siteLanguage={siteLanguage}
+        siteLocale={siteLocale}
+        twitterUsername={twitterUsername}
+        author={authorName}
+        article={true}
+        publishedDate={createdDate}
+        modifiedDate={updatedDate}
+      />
+      <h1 className="text-3xl semi-bold mb-4">
+        {data.contentfulBlogPost.title}
+      </h1>
       <TimeTag time={data.contentfulBlogPost.updatedAt} />
       <figure className="mb-8">
-        <img src={data.contentfulBlogPost.heroImage && data.contentfulBlogPost.heroImage.file.url} />
+        <img
+          src={
+            data.contentfulBlogPost.heroImage &&
+            data.contentfulBlogPost.heroImage.file.url
+          }
+        />
       </figure>
       <div className="mt-8">
         {documentToReactComponents(
@@ -28,11 +51,16 @@ export const query = graphql`
       body {
         json
       }
+<<<<<<< HEAD
       heroImage {
         title
         file {
           url
         }
+=======
+      teaser {
+        teaser
+>>>>>>> feat: Add SEO component
       }
       title
       updatedAt(formatString: "DD MMM YYYY")
