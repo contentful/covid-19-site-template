37ed5438d50cf57d880762131bc52cb91cbc26b3
Khaled Garbaya <khaled@contentful.com>
2020-03-18 16:29:27 +0100

    demo deploy (#22)
    * fix: reduce image size
    
    * Move SEO to Contentful
    
    * Add customizable background and background color
    
    * feat: Add Footer (#19)
    
    Co-authored-by: David Litvak Bruno <david.litvak@contentful.com>
    
    Co-authored-by: David Litvak <david.litvakb@gmail.com>
    Co-authored-by: David Litvak Bruno <david.litvak@contentful.com>

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@ import FeaturedArticles from './featuredArticles'
 import Nav from './nav'
 
 const Layout = ({ children }) => {
-  const { stats, tags, featuredArticles } = useStaticQuery(graphql`
+  const { stats, tags, featuredArticles, meta} = useStaticQuery(graphql`
     {
       stats: allCovid19CountrySummary {
         nodes {
@@ -33,24 +33,49 @@ const Layout = ({ children }) => {
           }
         }
       }
+      meta: allContentfulSiteMetadata {
+        nodes {
+          backgroundColor
+          backgroundImage {
+            unsplashUrl {
+             unsplashUrl
+            }
+          }
+        }
+      }
     }
   `)
+
+  const backgroundColor = meta.nodes[0].backgroundColor || 'bg-blue-500'
+  const unsplashUrl = meta.nodes[0].backgroundImage && meta.nodes[0].backgroundImage.unsplashUrl.unsplashUrl
+
   return (
     <>
       <header className="relative">
         <Nav />
       </header>
-      <div role="banner" className="h-banner bg-blue-500"></div>
-      <main className="flex flex-wrap bg-white shadow-lg container max-w-screen-xl mx-auto mt-banner">
-        <section className="w-full py-8 px-6 md:w-2/3">{children}</section>
+      <div role="banner" className={`h-banner ${backgroundColor}`} style={unsplashUrl && {'background-image': `url(${unsplashUrl})`, 'background-size': 'cover'}}></div>
+      <main className="container flex flex-wrap mx-auto bg-white shadow-lg max-w-screen-xl mt-banner">
+        <section className="w-full px-6 py-8 md:w-2/3">{children}</section>
         <aside className="w-full px-6 py-8 bg-gray-100 md:w-1/3">
-          <FeaturedArticles featuredArticles={featuredArticles.nodes[0].blogPosts} />
+           <FeaturedArticles
+            featuredArticles={featuredArticles.nodes[0].blogPosts}
+          />
           <TagList tags={tags.nodes} />
           {stats.nodes.map(summary => (
             <Stat summary={summary} key={summary.country} />
           ))}
         </aside>
       </main>
+      <footer className="container px-6 py-8 mx-auto text-center bg-gray-100 max-w-screen-xl">
+        Create your own Covid-19 announcement website on{' '}
+        <a
+          className="text-blue-700 hover:text-blue-900 text-sm no-underline hover:underline"
+          href="https://stop-the-spread.dev"
+        >
+          stop-the-spread.dev
+        </a>
+      </footer>
     </>
   )
 }
