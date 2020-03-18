af8cc86c1527060c4363c2198827b6b8bbfaffb3
Khaled Garbaya <khaled@contentful.com>
2020-03-18 08:53:52 +0100

    fix: reduce image size

--- src/utils/rich-text-render-options.js
+++ src/utils/rich-text-render-options.js
@@ -4,22 +4,32 @@ import { BLOCKS, INLINES } from '@contentful/rich-text-types'
 const Text = ({ children }) => (
   <p className="my-2 font-sans text-lg">{children}</p>
 )
-const H1 = ({ children }) => <h1 className="font-sans text-2xl font-semi-bold">{children}</h1>
-const H2 = ({ children }) => <h2 className="font-sans text-xl font-semi-bold">{children}</h2>
-const H3 = ({ children }) => <h3 className="font-sans text-lg font-semi-bold">{children}</h3>
-const H4 = ({ children }) => <h4 className="font-sans text-base font-semi-bold">{children}</h4>
+const H1 = ({ children }) => (
+  <h1 className="font-sans text-2xl font-semi-bold">{children}</h1>
+)
+const H2 = ({ children }) => (
+  <h2 className="font-sans text-xl font-semi-bold">{children}</h2>
+)
+const H3 = ({ children }) => (
+  <h3 className="font-sans text-lg font-semi-bold">{children}</h3>
+)
+const H4 = ({ children }) => (
+  <h4 className="font-sans text-base font-semi-bold">{children}</h4>
+)
 const HYPERLINK = ({ children, dataUri }) => (
-  <a className="text-blue-700 hover:text-blue-900 underline text-sm" href={dataUri}>
+  <a
+    className="text-blue-700 hover:text-blue-900 underline text-sm"
+    href={dataUri}
+  >
     {children}
   </a>
 )
-const Img = ({ node }) =>
-  console.log(node) || (
-    <img
-      src={node.data.target.fields.file['en-US'].url}
-      alt={node.data.target.fields.title['en-US']}
-    />
-  )
+const Img = ({ node }) => (
+  <img
+    src={`https:${node.data.target.fields.file['en-US'].url}?w=630&h=310&fit=thumb`}
+    alt={node.data.target.fields.title['en-US']}
+  />
+)
 const options = {
   renderNode: {
     [BLOCKS.PARAGRAPH]: (_node, children) => <Text>{children}</Text>,
@@ -28,7 +38,9 @@ const options = {
     [BLOCKS.HEADING_3]: (_node, children) => <H3>{children}</H3>,
     [BLOCKS.HEADING_4]: (_node, children) => <H4>{children}</H4>,
     [BLOCKS.EMBEDDED_ASSET]: node => <Img node={node} />,
-    [INLINES.HYPERLINK]: (_node, children) => <HYPERLINK dataUri={_node.data && _node.data.uri}>{children}</HYPERLINK>,
+    [INLINES.HYPERLINK]: (_node, children) => (
+      <HYPERLINK dataUri={_node.data && _node.data.uri}>{children}</HYPERLINK>
+    ),
   },
   renderText: text => text.replace('!', '?'),
 }
