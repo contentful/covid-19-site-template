5aa26390c916bdd5a1d78222b2931608537e20e0
Simon Hanukaev <semenh@gmail.com>
2020-03-27 17:55:21 +0300

    Move countries to Contentful
    - added country codes to site metadata
    - added get-countries.js script to fetch country codes from contentful and store them as country-codes.json
    - added get-countries.js to npm scripts to run before running gatsby develop and gatsby build
    - udpated gatsby-config.js to read contry codes from country-codes.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-mathdroid-covid19',
       options: {
-        countries: [{ iso2: process.env.COVID_COUNTRY }],
+        countries: require('./country-codes.json').map(countryCode => ({ iso2: countryCode })),
       },
     },
     {
---
+++ get-countries.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  'method': 'GET',
+  'hostname': 'cdn.contentful.com',
+  'path': `/spaces/${process.env.CF_COVID_SPACE_ID}/environments/master/entries?content_type=siteMetadata&select=fields.covidCountries`,
+  'headers': { 'Authorization': `Bearer ${process.env.CF_COVID_ACCESS_TOKEN}` }
+};
+
+const req = https.request(options, (res) => {
+  let data = '';
+
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('end', () => {
+    try {
+      data = JSON.parse(data);
+      const countries = data.items[0].fields.covidCountries;
+      fs.writeFileSync('country-codes.json', JSON.stringify(countries));
+      process.exit(0);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  });
+
+});
+
+req.on('error', error => {
+  console.error(error);
+  process.exit(1);
+})
+
+req.end();
